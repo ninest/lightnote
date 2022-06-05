fa90aa1d6719265e9dcd16d070c20bae9fbbe408
ninest <parth.kabra@gmail.com>
2022-06-05 22:47:27 +0800

    Add state

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,8 +1,15 @@
 import Placeholder from "@tiptap/extension-placeholder";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { useAtom } from "jotai";
+import { atomWithHash } from "jotai/utils";
+import { useEffect } from "react";
+
+const contentAtom = atomWithHash("content", "");
 
 export const TextEditor = () => {
+  const [content, setContent] = useAtom(contentAtom);
+
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -10,8 +17,12 @@ export const TextEditor = () => {
         placeholder: "Start writing ...",
       }),
     ],
-    content: "",
+    content,
   });
+
+  useEffect(() => {
+    setContent(editor?.getHTML() || "");
+  }, [editor?.state]);
 
   return (
     <div className="h-full mx-auto pt-xl pb-[30%] w-full px-base md:w-4/6 md:px-0 max-w-[80ch]">
