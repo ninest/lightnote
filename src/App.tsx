4c074f1328557349c7a7b8db7cfb0f234b7bf62d
ninest <parth.kabra@gmail.com>
2022-06-05 22:39:26 +0800

    Style editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
 import { TextEditor } from "./components/TextEditor";
 
 function App() {
   return (
     <>
-      <h1>Lighter notes</h1>
-      <TextEditor />
+      <main className="h-full">
+        <TextEditor />
+      </main>
     </>
   );
 }
--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,16 +1,21 @@
+import Placeholder from "@tiptap/extension-placeholder";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
 export const TextEditor = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      Placeholder.configure({
+        placeholder: "Start writing ...",
+      }),
+    ],
     content: "",
   });
 
   return (
-    <EditorContent
-      editor={editor}
-      placeholder="Start writing here ..."
-    ></EditorContent>
+    <div className="h-full mx-auto pt-xl pb-[30%] w-full px-base md:w-4/6 md:px-0 max-w-[80ch]">
+      <EditorContent className="h-full prose" editor={editor}></EditorContent>
+    </div>
   );
 };
